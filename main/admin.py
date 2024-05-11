13f7185c3e073da58c659881e9e6436ff9bfee61
Repin Andrei <repin_andrei@list.ru>
2024-05-11 20:14:34 +0300

    Create project:app and Create applicate:main and Error404

---
+++ main/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+#from main.models import Person
+#Данный импорт нужен для того, чтобы наша таблица отображалась в admin панеле
+from main.models import Categories, Products
+
+#admin.site.register(Categories)
+#admin.site.register(Products)
+
+"""
+В данном классе можно настроить тонкую настройку отображения
+информации в admin панеле
+"""
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    # prepopulated_fields={'slug':('name',)}
+    fields_display=('name',)
+    ordering=('name',)
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    # prepopulated_fields={'slug':('name',)}
+    ordering=('name',)
+
+
+# @admin.register(Person)
+# class PersonAdmin(admin.ModelAdmin):
+# display= ('name', 'surname',)
+# serch_fields=('name', 'surname',)
+# ordering=('name',)
+# # Register your models here.
+
---
+++ main/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from main.models import Categories
+
+# def index(request):
+# person=Person.objects.all()
+# next_page_url=reverse("main:next_page")
+# context={
+# 'Login': 'Таблица',
+# 'description': 'Список пользователей',
+# 'person': person,
+# 'next_page_url' : next_page_url,
+# }
+# return render(request, "main/main.html", context)
+def index(request):
+    try:
+        cate = Categories.objects.all()
+        return render(request, "main/main.html", {"cate": cate})
+    except cate.DoesNotExist:
+        context={
+            "message_error": "<h2>Not found page :(</h2>",
+        }
+        return render(request, "404.html", context)
+
+def create(request):
+    if request.method=="POST":
+        cate=Categories()
+        cate.name=request.POST.get("name")
+        cate.name_two=request.POST.get("name_two")
+        cate.save()
+    #return render(request, "main/main.html")
+    return HttpResponseRedirect('/')
+
+def edit(request, id):
+    try:
+        cate=Categories.objects.get(id=id)
+        if request.method=="POST":
+            cate.name=request.POST.get("name")
+            cate.name_two=request.POST.get("name_two")
+            cate.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "main/next.html", {"cate" : cate})
+
+    except cate.DoesNotExist:
+        return HttpResponseRedirect("<h2>Not found page :(</h2>")
+
+def delete(request, id):
+    try:
+        cate=Categories.objects.get(id=id)
+        cate.delete()
+        return HttpResponseRedirect("/")
+    except cate.DoesNotExist:
+        return HttpResponseRedirect("<h2>Not found page 😦 </h2>")
+
+# def next_page(request):
+# index_url=reverse("main:index")
+# context={
+# 'index_url' : index_url,
+# }
+# return render(request, "main/next.html", context)
+
+# Create your views here.
+
